bad3c6c6891bc5206ffe01c8c4d9f342670b9925
Ricardo Colares <ricardoccolares@gmai.com>
2021-03-14 10:23:34 -0300

    pagina retornando dados da api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,27 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem.jsx";
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
+import "../styles/repositories.scss";
 
 export function RepositoryLIst() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/ricardocolares/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
-    <section>
+    <section className="repository-list">
       <h1>Lista de repositorios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
